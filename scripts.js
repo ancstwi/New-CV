888639ebc523a62e62c9dbb45b7a7305d74ab167
ancstwi <62105882+ancstwi@users.noreply.github.com>
2022-04-26 23:36:21 +0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -17,3 +17,16 @@ if (cross) {
         menuBlock.classList.remove('active');
     });
 }
+
+const links = document.querySelectorAll('a[href^="#"]');
+for (let link of links) {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        const id = link.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
